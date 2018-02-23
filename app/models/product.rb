c57f99a277310d5a472400e95274f4101e017dfa
Awlter <wxfwalter@gmail.com>
2018-02-23 10:34:33 +0800

    Iteration D2: Connecting Products to Carts

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates_presence_of :title, :description, :price, :image_url
   validates_numericality_of :price, greater_than_or_equal_to: 0.01
   validates_uniqueness_of :title
@@ -7,4 +11,12 @@ class Product < ApplicationRecord
     message: "must be a URL for GIF, JPG, or PNG image."
   }
 
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Items present')
+      throw(:abort)
+    end
+  end
 end

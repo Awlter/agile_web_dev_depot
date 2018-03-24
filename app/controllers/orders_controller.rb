bf6904472cb64b3e29ab1d6c22b980bc149584f6
Awlter <wxfwalter@gmail.com>
2018-03-24 13:18:31 +0800

    G1: add Order model and controller, learn how to declare an enum attribute

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,13 @@
+class OrdersController < ApplicationController
+  include CurrentCart
+  before_action :set_cart, only: [:new, :create]
+  before_action :ensure_cart_isnt_empty, only: :new
+
+  private
+
+  def ensure_cart_isnt_empty
+    if @cart.line_items.empty?
+      redirect_to store_index_url, notice: 'Your cart is empty.'
+    end
+  end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
   root 'store#index', as: "store_index"
 
   resources :products
+  resources :orders, only: [:new]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

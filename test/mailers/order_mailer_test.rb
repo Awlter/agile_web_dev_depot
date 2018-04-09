f9d959d2a361125f04f9f0e58dd2540e4461dfa6
Awlter <wxfwalter@gmail.com>
2018-04-09 16:40:25 +0800

    Iteration I1: action mailer with a bug of `protect_against_forgery?` when an action mailer view template trying to render a line item partial

---
+++ test/mailers/order_mailer_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class OrderMailerTest < ActionMailer::TestCase
+  test "received" do
+    mail = OrderMailer.received(orders(:one))
+    assert_equal 'Pragmatic Store Order Confirmation', mail.subject
+    assert_equal ["wxfwalter@example.com"], mail.to
+    assert_equal ["wxfwalter@gmail.com"], mail.from
+  end
+
+  # test "shipped" do
+  #   mail = OrderMailer.shipped
+  #   assert_equal "Shipped", mail.subject
+  #   assert_equal ["to@example.org"], mail.to
+  #   assert_equal ["from@example.com"], mail.from
+  #   assert_match "Hi", mail.body.encoded
+  # end
+
+end

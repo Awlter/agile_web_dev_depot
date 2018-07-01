743371993d4ccbee442ac64e4a8bb59165637bac
Awlter <wxfwalter@gmail.com>
2018-07-01 16:55:29 +0800

    add ship date column to orders table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410144827) do
+ActiveRecord::Schema.define(version: 20180701084452) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20180410144827) do
     t.integer "pay_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "ship_date"
   end
 
   create_table "products", force: :cascade do |t|

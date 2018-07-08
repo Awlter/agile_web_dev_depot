3933796543d52bea500995eadebf45b62f096faa
Awlter <wxfwalter@gmail.com>
2018-07-08 23:17:02 +0800

    play time

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,15 @@ class ApplicationController < ActionController::Base
   protected
 
   def authorize
-    unless User.find_by(id: session[:user_id])
-      redirect_to login_url, notice: 'Please log in'
+    if request.format.html?
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: 'Please log in'
+      end
+    else
+      authenticate_or_request_with_http_basic do |id, password|
+        user = User.find_by(id: id)
+        render plain: 'failed' if !user || !user.validate(password)
+      end
     end
   end
 end
